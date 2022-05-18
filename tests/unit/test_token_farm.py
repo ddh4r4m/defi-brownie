cd0d0bfd04f736cad3c98994ebf92c7ec20d730d
rkstrdee <rkstrdee@gmail.com>
2022-05-18 14:24:32 +0530

    first test added

---
+++ tests/unit/test_token_farm.py
@@ -0,0 +1,24 @@
+from brownie import network
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    get_contract,
+)
+import pytest
+from scripts.deploy import deploy_token_farm_and_dharma_token
+
+
+def test_set_price_feed_contract():
+    # Arrange stage
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+    account = get_account()
+    non_owner = get_account(index=1)
+    token_farm, dharma_token = deploy_token_farm_and_dharma_token()
+    # Act Phase
+    price_feed_address = get_contract("eth_usd_price_feed")
+    token_farm.setPriceFeedContract(
+        dharma_token.address, price_feed_address, {"from": account}
+    )
+    # Assert Phase
+    assert token_farm.tokenPriceFeedMapping(dharma_token.address) == price_feed_address
